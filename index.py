310fbe272bf5cb88272dce94b8c882bbd603966e
Yuriy Chernyshov <georgthegreat@gmail.com>
2013-01-11 23:12:17 +0400

    Implemented logging. Fixed paragraphs render error, made all string in index.py unicode

--- index.py
+++ index.py
@@ -5,91 +5,107 @@ from data import *
 import os.path
 import sys
 
+import logging
+from logging import Formatter
+
 theApp = Flask(__name__)
 theApp.debug = True
 theApp.jinja_env.trim_blocks = True
 
-if (not os.path.exists("templates")):
-	print("Should run from root folder")
+if (not os.path.exists(u"templates")):
+	print(u"Should run from root folder")
 	sys.exit()
 
-@theApp.route("/")
+@theApp.route(u"/")
 def root():
-	return render_template("index.html", paragraphs=paragraphs)
+	return render_template(u"index.html", paragraphs=paragraphs)
 
-@theApp.route("/index.html")
+@theApp.route(u"/index.html")
 def anotherRoot():
-	return render_template("index.html", paragraphs=paragraphs)
+	return render_template(u"index.html", paragraphs=paragraphs)
 
-@theApp.route("/blog.html")
+@theApp.route(u"/blog.html")
 def blog():
-	return render_template("blog.html", blogImages=blogImages)
+	return render_template(u"blog.html", blogImages=blogImages)
 
-@theApp.route("/my.html")
+@theApp.route(u"/my.html")
 def my():
-	return render_template("my.html", impressionImages=impressionImages, marchImages=marchImages, photopImages=photopImages, drugoeImages=drugoeImages)
+	return render_template(u"my.html", impressionImages=impressionImages, marchImages=marchImages, photopImages=photopImages, drugoeImages=drugoeImages)
 
-@theApp.route("/celtic.html")
+@theApp.route(u"/celtic.html")
 def celtic():
-	return render_template("celtic.html", programs=programs)
+	return render_template(u"celtic.html", programs=programs)
 
-@theApp.route("/about.html")
+@theApp.route(u"/about.html")
 def about():
-	return render_template("about.html")
+	return render_template(u"about.html")
 
 #rss feeds
-@theApp.route("/rss/paragraphs.xml")
+@theApp.route(u"/rss/paragraphs.xml")
 def paragraphs_rss():
-	return render_template("rss/paragraphs.xml", paragraphs=reversed(paragraphs))
+	return render_template(u"rss/paragraphs.xml", paragraphs=reversed(paragraphs))
 
-@theApp.route("/rss/celtic.xml")
+@theApp.route(u"/rss/celtic.xml")
 def celtic_rss():
-	return render_template("rss/celtic.xml", programs=reversed(programs)	)
+	return render_template(u"rss/celtic.xml", programs=reversed(programs)	)
 
 #numbered content
-@theApp.route("/paragraphs/<string:index>.html")
+@theApp.route(u"/paragraphs/<string:index>.html")
 def paragraph_text(index):
 	try:
 		intIndex=int(index)
 	except ValueError:
-		raise NotFound()
+		return http_not_found(404)
 
-	if (os.path.isfile("templates/paragraphs/" + index + ".html") and intIndex < len(paragraphs)):
-		return render_template("paragraph_text.html", intIndex=intIndex, paragraphs=paragraphs)
+	if (os.path.isfile(u"templates/paragraphs/" + index + u".html") and intIndex < len(paragraphs)):
+		return render_template(u"paragraph_text.html", intIndex=intIndex, paragraphs=paragraphs)
 	else:
-		raise NotFound()
+		return http_not_found(404)
 
-@theApp.route("/celtic/<string:index>.html")
+@theApp.route(u"/celtic/<string:index>.html")
 def celtic_text(index):
 	try:
 		intIndex=int(index)
 	except ValueError:
-		raise NotFound()
+		return http_not_found(404)
 
-	if (os.path.isfile("templates/celtic/" + index + ".html") and intIndex <= len(programs)):
-		return render_template("celtic_text.html", intIndex=int(index), programs=programs)
+	if (os.path.isfile(u"templates/celtic/" + index + u".html") and intIndex <= len(programs)):
+		return render_template(u"celtic_text.html", intIndex=int(index), programs=programs)
 	else:
-		raise NotFound()
+		return http_not_found(404)
 
 #static content
-@theApp.route("/other/<path:filename>")
+@theApp.route(u"/other/<path:filename>")
 def other(filename):
-	return render_template("other/" + filename)
+	return render_template(u"other/" + filename)
 
-@theApp.route("/<path:filename>")
+@theApp.route(u"/<path:filename>")
 def static(filename):
-	if (os.path.isfile("static/" + filename)):
+	if (os.path.isfile(u"static/" + filename)):
 		return theApp.send_static_file(filename)
 	
 #setting error handlers
 @theApp.errorhandler(404)
 def http_not_found(error):
-	return render_template("404.html"), 404
+	theApp.logger.error(u"Error #404 occured")
+	return render_template(u"404.html"), 404
 
 @theApp.errorhandler(403)
-def http_not_found(error):
-	return render_template("403.html"), 403
+def http_forbidden(error):
+	theApp.logger.error(u"Error #403 occured")
+	return render_template(u"403.html"), 403
 
+file_handler = logging.FileHandler(u"leftparagraphs.log", mode=u"a", encoding=None, delay=False)
+file_handler.setFormatter(Formatter(
+'''
+Message type:       %(levelname)s
+Location:           %(pathname)s:%(lineno)d
+Message: %(message)s
+
+'''))
+file_handler.setLevel(logging.WARNING)
+theApp.logger.addHandler(file_handler)
+	
 if __name__ == "__main__":
 	theApp.run(host="0.0.0.0")
 
